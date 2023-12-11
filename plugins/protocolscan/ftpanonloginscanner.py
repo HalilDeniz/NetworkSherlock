3aaa39c29a954a1b58492dbf1b7616497227ab08
halil ibrahim deniz <62566319+HalilDeniz@users.noreply.github.com>
2023-12-11 12:42:52 +0300

    Add files via upload
    Signed-off-by: halil ibrahim deniz <62566319+HalilDeniz@users.noreply.github.com>

---
+++ plugins/protocolscan/ftpanonloginscanner.py
@@ -0,0 +1,29 @@
+import socket
+
+class FtpAnonLoginScanner:
+    def __init__(self, target, port=21, timeout=5):
+        self.target = target
+        self.port = port
+        self.timeout = timeout
+
+    def check_anon_login(self):
+        try:
+            with socket.create_connection((self.target, self.port), timeout=self.timeout) as sock:
+                sock.settimeout(self.timeout)
+                response = self._recv_response(sock)
+                if "220" in response:
+                    sock.sendall(b"USER anonymous\r\n")
+                    response = self._recv_response(sock)
+                    if "331" in response:  # Şifre isteniyor mu kontrol et
+                        sock.sendall(b"PASS anonymous\r\n")
+                        response = self._recv_response(sock)
+                        if "230" in response:
+                            return True
+        except socket.timeout:
+            pass
+        except socket.error as e:
+            pass
+        return False
+
+    def _recv_response(self, sock):
+        return sock.recv(4096).decode('utf-8', 'ignore').strip()
